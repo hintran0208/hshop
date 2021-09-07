b34e7985df314737ad65160613771b6ef426d611
Hien Tran <tranthanhhien5@gmail.com>
2021-09-07 22:08:32 +0700

    add to do list

--- src/App.js
+++ src/App.js
@@ -1,64 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import TodoFeature from './features/Todo';
 
 function App() {
-  const name = 'Hien Tran';
-  const age = 18;
-  const isMale = true;
-  const student = {
-    name: 'Hien Tran'
-  };
-  const colorList = ['red', 'green', 'blue']
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hien Tran
-        </p>
-
-        <p>Xin chao {name} - {age} - {isMale ? 'Male' : 'Female'}</p>
-
-        {isMale ? <p>Male</p> : <p>Female</p>}
-
-        {isMale && <p>Male</p>}
-
-        {!isMale && <p>Female</p>}
-
-        {isMale && (
-          <div>
-            <p>Male 1</p>
-            <p>Male 2</p>
-            <p>Male 3</p>
-          </div>
-        )}
-
-        {isMale && (
-          <React.Fragment>
-            <p>Male 1</p>
-            <p>Male 2</p>
-            <p>Male 3</p>
-          </React.Fragment>
-        )}
-
-        {isMale && (
-          <>
-            <p>Male 1</p>
-            <p>Male 2</p>
-            <p>Male 3</p>
-          </>
-        )}
-
-        <p>{student.name}</p>
-
-        <ul>
-          {colorList.map(color => (
-            <li style={{ color }}>{color}</li>
-          ))}
-        </ul>
-      </header>
+      <TodoFeature />
     </div>
   );
 }
--- src/features/Todo/index.jsx
+++ src/features/Todo/index.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import TodoList from './components/Todolist';
 
 TodoFeature.propTypes = {
     
 };
 
 function TodoFeature(props) {
+    const todoList = [
+        {
+            id: 1,
+            title: 'Eat'
+        },
+        {
+            id: 2,
+            title: 'Study'
+        },
+        {
+            id: 3,
+            title: 'Sleep'
+        }
+    ]
+
     return (
         <div>
-            
+            <h3>To Do List</h3>
+            <TodoList todoList={todoList}/>
         </div>
     );
 }
