c80c978099e33a9ed988d7f04eaebe1e7aa1197c
Hien Tran <tranthanhhien5@gmail.com>
2021-09-08 00:25:11 +0700

    add album

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-// import './App.css';
-import TodoFeature from './features/Todo';
+import AlbumFeature from './features/Album';
 
 function App() {
   return (
     <div className="App">
-      <TodoFeature />
+      <AlbumFeature />
     </div>
   );
 }
---
+++ src/features/Album/index.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import AlbumList from './components/AlbumList';
+
+AlbumFeature.propTypes = {
+
+};
+
+function AlbumFeature(props) {
+    const albumList = [
+        {
+            id: 1,
+            name: 'Nhóm Nhạc Yêu Thích',
+            thumbnailUrl: 'https://photo-resize-zmp3.zadn.vn/w320_r1x1_jpeg/cover/2/f/9/6/2f96e5dbeb36dd83781d764417baa164.jpg'
+        },
+        {
+            id: 2,
+            name: 'Back To 90\'s',
+            thumbnailUrl: 'https://photo-resize-zmp3.zadn.vn/w320_r1x1_jpeg/cover/4/4/b/4/44b42aa16ce3055b45a60a681827e64e.jpg'
+        },
+        {
+            id: 3,
+            name:'Top 100 Pop Âu Mỹ Hay Nhất',
+            thumbnailUrl: 'https://photo-resize-zmp3.zadn.vn/w320_r1x1_jpeg/cover/5/d/8/9/5d890d445e5ecff4d118a296440f1654.jpg'
+        }
+    ]
+    return (
+        <div>
+            <h3>Có thể bạn sẽ thích</h3>
+            <AlbumList albumList = {albumList}/>
+        </div>
+    );
+}
+
+export default AlbumFeature;
