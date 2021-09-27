fc946294e2adcb1e1840276fdc2b86390e0289d2
Hien Tran <tranthanhhien5@gmail.com>
2021-09-27 14:41:23 +0700

    add route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Route } from 'react-router';
 import TodoFeature from './features/Todo';
+import AlbumsFeature from './features/Album';
 
 function App() {
   return (
-    <div className="App">
-      <TodoFeature />
+    <div className='App'>
+      Header
+      <Route path='/todos' component={TodoFeature} />
+      <Route path='/albums' component={AlbumsFeature} />
+      Footer
     </div>
   );
 }
--- src/features/Todo/index.jsx
+++ src/features/Todo/index.jsx
@@ -2,75 +2,75 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import TodoList from './components/Todolist';
 
-TodoFeature.propTypes = {
-    
-};
+TodoFeature.propTypes = {};
 
 function TodoFeature(props) {
-    const initTodoList = [
-        {
-            id: 1,
-            title: 'Eat',
-            status: 'new'
-        },
-        {
-            id: 2,
-            title: 'Study',
-            status: 'completed'
-        },
-        {
-            id: 3,
-            title: 'Sleep',
-            status: 'new'
-        }
-    ]
+  const initTodoList = [
+    {
+      id: 1,
+      title: 'Eat',
+      status: 'new',
+    },
+    {
+      id: 2,
+      title: 'Study',
+      status: 'completed',
+    },
+    {
+      id: 3,
+      title: 'Sleep',
+      status: 'new',
+    },
+  ];
 
-    const [todoList, setTodoList] = useState(initTodoList);
-    const [filteredStatus, setFilteredStatus] = useState('all');
+  const [todoList, setTodoList] = useState(initTodoList);
+  const [filteredStatus, setFilteredStatus] = useState('all');
 
-    const handleTodoClick = (todo, idx) => {
-        // Clone todo list to new one
-        const newTodoList = [...todoList];
+  const handleTodoClick = (todo, idx) => {
+    // Clone todo list to new one
+    const newTodoList = [...todoList];
 
-        // Toggle State
-        const newTodo = {
-            ...newTodoList[idx],
-            status: newTodoList[idx].status === 'new' ? 'completed' : 'new',
-        }
+    // Toggle State
+    const newTodo = {
+      ...newTodoList[idx],
+      status: newTodoList[idx].status === 'new' ? 'completed' : 'new',
+    };
 
-        newTodoList[idx] = newTodo;
+    newTodoList[idx] = newTodo;
 
-        // Update todo list
-        setTodoList(newTodoList);
-    }
+    // Update todo list
+    setTodoList(newTodoList);
+  };
 
-    const handleShowAllClick = () => {
-        setFilteredStatus('all');
-    }
+  const handleShowAllClick = () => {
+    setFilteredStatus('all');
+  };
 
-    const handleShowCompletedClick = () => {
-        setFilteredStatus('completed');
-    }
+  const handleShowCompletedClick = () => {
+    setFilteredStatus('completed');
+  };
 
-    const handleShowNewClick = () => {
-        setFilteredStatus('new');
-    }
+  const handleShowNewClick = () => {
+    setFilteredStatus('new');
+  };
 
-    const renderTodoList = todoList.filter (todo => filteredStatus === 'all' || filteredStatus === todo.status)
-    // console.log(renderTodoList);
+  const renderTodoList = todoList.filter(
+    (todo) => filteredStatus === 'all' || filteredStatus === todo.status
+  );
+  // console.log(renderTodoList);
 
-    return (
-        <div>
-            <h3>To Do List</h3>
-            <TodoList todoList={renderTodoList} onTodoClick={handleTodoClick}/>
-       
-            <div>
-                <button onClick={handleShowAllClick}>Show All</button>
-                <button onClick={handleShowCompletedClick}>Show Completed</button>
-                <button onClick={handleShowNewClick}>Show New</button>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <h3>To Do List</h3>
+      <TodoList todoList={renderTodoList} onTodoClick={handleTodoClick} />
+
+      <div>
+        <button onClick={handleShowAllClick}>Show All</button>
+        <button onClick={handleShowCompletedClick}>Show Completed</button>
+        <button onClick={handleShowNewClick}>Show New</button>
+      </div>
+    </div>
+  );
 }
 
 export default TodoFeature;
