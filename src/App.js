8b2fa96dc801c78dec60d6159d5e05216e62081c
Hien Tran <tranthanhhien5@gmail.com>
2021-09-27 16:09:42 +0700

    add redirect

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React from 'react';
-import { Route } from 'react-router';
+import { Redirect, Route, Switch } from 'react-router';
 import TodoFeature from './features/Todo';
 import AlbumsFeature from './features/Album';
+import { Link, NavLink } from 'react-router-dom';
 
 function App() {
   return (
     <div className='App'>
       Header
-      <Route path='/todos' component={TodoFeature} />
-      <Route path='/albums' component={AlbumsFeature} />
+      <p>
+        <NavLink to='/todos' activeClassName='active-menu'>
+          Todo
+        </NavLink>
+      </p>
+      <p>
+        <NavLink to='/albums'>Albums</NavLink>
+      </p>
+      <Switch>
+        <Redirect from='/home' to='/' exact />
+        <Redirect from='/post-list/:postId' to='/posts/:postId' exact />
+        <Route path='/todos' component={TodoFeature} />
+        <Route path='/albums' component={AlbumsFeature} />
+      </Switch>
       Footer
     </div>
   );
