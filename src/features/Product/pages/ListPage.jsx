1b7b67fca87e7160ae1bb275e11ae666086b9bea
Hien Tran <tranthanhhien5@gmail.com>
2021-11-04 10:12:38 +0700

    127.Add pagination logic

--- src/features/Product/pages/ListPage.jsx
+++ src/features/Product/pages/ListPage.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { Box } from '@mui/system';
-import { Container, Grid, Paper, Typography } from '@mui/material';
+import { Container, Grid, Pagination, Paper, Typography } from '@mui/material';
 import productApi from 'api/productApi';
 import ProductSkeletonList from '../components/ProductSkeletonList';
 import ProductList from '../components/ProductList';
@@ -10,20 +10,37 @@ ListPage.propTypes = {};
 
 function ListPage(props) {
   const [productList, setProductList] = useState([]);
+  const [pagination, setPagination] = useState({
+    limit: 9,
+    total: 10,
+    page: 1,
+  });
   const [loading, setLoading] = useState(true);
+  const [filters, setFilters] = useState({
+    _page: 1,
+    _limit: 9,
+  });
 
   useEffect(() => {
     (async () => {
       try {
-        const { data } = await productApi.getAll({ _page: 1, _limit: 10 });
+        const { data, pagination } = await productApi.getAll(filters);
         setProductList(data);
+        setPagination(pagination);
       } catch (error) {
         console.log('Failed to fetch product list: ', error);
       }
 
       setLoading(false);
     })();
-  }, []);
+  }, [filters]);
+
+  const handlePageChange = (e, page) => {
+    setFilters((prevFilters) => ({
+      ...prevFilters,
+      _page: page,
+    }));
+  };
 
   return (
     <Box>
@@ -35,7 +52,14 @@ function ListPage(props) {
 
           <Grid item sx={{ flex: '1 1 0' }}>
             <Paper elevation={0}>
-              {loading ? <ProductSkeletonList /> : <ProductList data={productList} />}
+              {loading ? <ProductSkeletonList length={9} /> : <ProductList data={productList} />}
+
+              <Pagination
+                color="primary"
+                count={Math.ceil(pagination.total / pagination.limit)}
+                page={pagination.page}
+                onChange={handlePageChange}
+              ></Pagination>
             </Paper>
           </Grid>
         </Grid>
