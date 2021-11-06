ce8bd0f88ea377448dd5d5b2d8e6026e1f8bb63f
Hien Tran <tranthanhhien5@gmail.com>
2021-11-06 08:41:06 +0700

    138.Add usememo to imporve performance

--- src/features/Product/components/FilterViewer.jsx
+++ src/features/Product/components/FilterViewer.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@mui/styles';
 import { Box } from '@mui/system';
@@ -97,9 +97,13 @@ FilterViewer.propTypes = {
 function FilterViewer({ filters = {}, onChange = null }) {
   const classes = useStyles();
 
+  const visibleFilter = useMemo(() => {
+    return FILTER_LIST.filter((x) => x.isVisible(filters));
+  }, [filters]);
+
   return (
     <Box component="ul" className={classes.root}>
-      {FILTER_LIST.filter((x) => x.isVisible(filters)).map((x) => (
+      {visibleFilter.map((x) => (
         <li key={x.id}>
           <Chip
             label={x.getLabel(filters)}
