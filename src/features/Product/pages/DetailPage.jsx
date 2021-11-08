dcae74319ec0b142cd70026112c189cdbf2db6ab
Hien Tran <tranthanhhien5@gmail.com>
2021-11-08 10:13:29 +0700

    151.Add menu sub-component

--- src/features/Product/pages/DetailPage.jsx
+++ src/features/Product/pages/DetailPage.jsx
@@ -1,11 +1,14 @@
 import { Container, Grid, Paper } from '@mui/material';
 import { Box } from '@mui/system';
 import React from 'react';
-import { useRouteMatch } from 'react-router';
+import { Route, Switch, useRouteMatch } from 'react-router';
 import AddToCartForm from '../components/AddToCartForm';
+import ProductAdditional from '../components/ProductAdditional';
+import ProductDescription from '../components/ProductDescription';
 import ProductDetailSkeleton from '../components/ProductDetailSkeleton';
 import ProductInfo from '../components/ProductInfo';
 import ProductMenu from '../components/ProductMenu';
+import ProductReviews from '../components/ProductReviews';
 import ProductThumbnail from '../components/ProductThumbnail';
 import useProductDetail from '../hooks/useProductDetail';
 
@@ -14,6 +17,7 @@ DetailPage.propTypes = {};
 function DetailPage(props) {
   const {
     params: { productId },
+    url,
   } = useRouteMatch();
 
   const { product, loading } = useProductDetail(productId);
@@ -42,6 +46,15 @@ function DetailPage(props) {
         </Paper>
 
         <ProductMenu />
+
+        <Switch>
+          <Route exact path={url}>
+            <ProductDescription product={product} />
+          </Route>
+
+          <Route path={`${url}/additional`} component={ProductAdditional} />
+          <Route path={`${url}/reviews`} component={ProductReviews} />
+        </Switch>
       </Container>
     </Box>
   );
